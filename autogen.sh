f13b573199bf3af409d1836e1e2078f97c45cc98
Philip Van Hoof <philip@codeminded.be>
2009-09-30 15:44:38 +0200

    Prepare build environment for Maemo compatibility
    Signed-off-by: Jannis Pohlmann <jannis@xfce.org>

--- autogen.sh
+++ autogen.sh
@@ -1,35 +1,24 @@
 #!/bin/sh
-#
-# vi:set et ai sw=2 sts=2 ts=2: */
-# -
-# Copyright (c) 2009 Jannis Pohlmann <jannis@xfce.org>
-# 
-# This program is free software; you can redistribute it and/or 
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 2 of 
-# the License, or (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public 
-# License along with this program; if not, write to the Free 
-# Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-# Boston, MA 02110-1301, USA.
+# Run this to generate all the initial makefiles, etc.
 
-(type xdt-autogen) >/dev/null 2>&1 || {
-  cat >&2 <<EOF
-autogen.sh: You don't seem to have the Xfce development tools installed on
-            your system, which are required to build this software.
-            Please install the xfce4-dev-tools package first, it is available
-            from http://www.xfce.org/.
-EOF
-  exit 1
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+PKG_NAME="tumbler"
+REQUIRED_AUTOMAKE_VERSION=1.9
+
+(test -f $srcdir/configure.ac \
+  && test -f $srcdir/README) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
 }
 
-# # initialize GTK-Doc
-# gtkdocize || exit 1
+# Automake requires that ChangeLog exist.
+touch ChangeLog
 
-XDT_AUTOGEN_REQUIRED_VERSION="4.7.2" exec xdt-autogen $@
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common from the GNOME CVS"
+    exit 1
+}
+. gnome-autogen.sh
