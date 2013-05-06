805fd767fe1785e4dbaf48c5c1c2d95f3ad04641
Nick Schermer <nick@xfce.org>
2013-05-06 18:35:28 +0200

    Only enable keepalive in curl >= 7.25.

--- plugins/cover-thumbnailer/cover-thumbnailer.c
+++ plugins/cover-thumbnailer/cover-thumbnailer.c
@@ -274,9 +274,12 @@ cover_thumbnailer_load_prepare (CoverThumbnailer *cover,
   curl_handle = curl_easy_init ();
   curl_multi_add_handle (cover->curl_multi, curl_handle);
   /* curl_easy_setopt (curl_handle, CURLOPT_VERBOSE, TRUE); */
-  curl_easy_setopt (curl_handle, CURLOPT_TCP_KEEPALIVE, TRUE);
   curl_easy_setopt (curl_handle, CURLOPT_URL, url);
   curl_easy_setopt (curl_handle, CURLOPT_USERAGENT, PACKAGE_NAME "/" PACKAGE_VERSION);
+
+#if LIBCURL_VERSION_NUM >= 0x071900 /* available since 7.25.0 */
+  curl_easy_setopt (curl_handle, CURLOPT_TCP_KEEPALIVE, TRUE);
+#endif
 
   /* cancellable check */
   curl_easy_setopt (curl_handle, CURLOPT_PROGRESSFUNCTION, cover_thumbnailer_check_progress);
