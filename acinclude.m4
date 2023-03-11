xfce-mirror/tumbler

commit 2f16e4171d64a74fd58ac8adb5a023dfca319b2d

cover-thumbnailer: Fix deprecation warning

acinclude.m4
@@ -264,7 +264,7 @@ if test x"$ac_tumbler_cover_thumbnailer" = x"yes"; then
   PKG_CHECK_MODULES([GDK_PIXBUF], [gdk-pixbuf-2.0 >= 2.40.0],
   [
     dnl Check for curl
-    PKG_CHECK_MODULES([CURL], [libcurl], [], [ac_tumbler_cover_thumbnailer=no])
+    PKG_CHECK_MODULES([CURL], [libcurl], [7.32.0], [ac_tumbler_cover_thumbnailer=no])
   ], [ac_tumbler_cover_thumbnailer=no])
 fi
 

plugins/cover-thumbnailer/cover-thumbnailer.c
@@ -232,7 +232,7 @@ cover_thumbnailer_load_prepare (CoverThumbnailer *cover,
 #endif
 
   /* cancellable check */
-  curl_easy_setopt (curl_handle, CURLOPT_PROGRESSFUNCTION, cover_thumbnailer_check_progress);
+  curl_easy_setopt (curl_handle, CURLOPT_XFERINFOFUNCTION, cover_thumbnailer_check_progress);
   curl_easy_setopt (curl_handle, CURLOPT_PROGRESSDATA, cancellable);
   curl_easy_setopt (curl_handle, CURLOPT_NOPROGRESS, FALSE);
 

commit 64a7103b8298780f24ddb7aee53a44526b70a07c

Fix previous commit
Fixes: 2f16e4171d64a74fd58ac8adb5a023dfca319b2d

acinclude.m4
@@ -264,7 +264,7 @@ if test x"$ac_tumbler_cover_thumbnailer" = x"yes"; then
   PKG_CHECK_MODULES([GDK_PIXBUF], [gdk-pixbuf-2.0 >= 2.40.0],
   [
     dnl Check for curl
-    PKG_CHECK_MODULES([CURL], [libcurl], [7.32.0], [ac_tumbler_cover_thumbnailer=no])
+    PKG_CHECK_MODULES([CURL], [libcurl >= 7.32.0], [], [ac_tumbler_cover_thumbnailer=no])
   ], [ac_tumbler_cover_thumbnailer=no])
 fi
 